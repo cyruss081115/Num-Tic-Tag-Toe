8269cf656307832df44f2bb073c4ca1672e34820
cyruss081115 <cyruss081115@gmail.com>
2022-05-24 01:47:59 +0800

    Initial Commit

---
+++ Num Tic-Tag-Toe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Num Tic-Tag-Toe
+//
+//  Created by Cyrus Wong on 24/5/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
